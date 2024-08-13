ff8d37efab4fb83afde674431ce085b82813c29e
isaias lima <izauiaslima@gmail.com>
2024-08-13 00:43:30 -0300

    V2.1 Modificação para o singular e mudança de ordem de criação das tabelas!

--- REDETEC_DATABASE.sql
+++ REDETEC_DATABASE.sql
@@ -1,130 +1,140 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema New_TCC
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema New_TCC
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `New_TCC` DEFAULT CHARACTER SET utf8 ;
 USE `New_TCC` ;
+
+-- -----------------------------------------------------
+-- Table `New_TCC`.`Usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `New_TCC`.`Usuario` (
+  `Id_Usuario` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `Nome_Usuario` VARCHAR(45) NOT NULL,
+  `CPF_Usuario` CHAR(11) NOT NULL,
+  `Data_Nascimento_Usuario` DATE NOT NULL,
+  `Cidade_Usuario` VARCHAR(45),
+  `Email_Usuario` VARCHAR(100) NOT NULL,
+  `Senha_Usuario` VARCHAR(100) NOT NULL,
+  `Sexo_Usuario` ENUM("F", "M"),
+  `Nivel_acesso` INT NOT NULL DEFAULT 1
+);
+
+
+-- -----------------------------------------------------
+-- Table `New_TCC`.`Curso`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `New_TCC`.`Curso` (
+  `id_Curso` INT NOT NULL AUTO_INCREMENT,
+  `Nome_Curso` VARCHAR(45) NOT NULL,
+  `Horario_Curso` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_Curso`));
+
 
 -- -----------------------------------------------------
 -- Table `New_TCC`.`Perfil`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `New_TCC`.`Perfil` (
-  `id_Perfil` INT NOT NULL AUTO_INCREMENT,
+  `Id_Perfil` INT NOT NULL AUTO_INCREMENT,
+  `Id_Usuario` INT NOT NULL,
   `Foto_perfil` VARCHAR(256) NULL,
   `Biografia_Perfil` TEXT NULL,
-  `Data_atualizacao_Perfil` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_Perfil`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `New_TCC`.`Usuarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `New_TCC`.`Usuarios` (
-  `id_Usuarios` INT NOT NULL AUTO_INCREMENT,
-  `id_Perfil` INT NOT NULL,
-  `Nome_Usuario` VARCHAR(45) NOT NULL,
-  `CPF_Usuario` CHAR(11) NOT NULL,
-  `Data_Nascimento_Usuario` DATE NOT NULL,
-  `Cidade_Usuario` VARCHAR(45) NULL,
-  `Email_Usuario` VARCHAR(100) NOT NULL,
-  `Senha_Usuario` VARCHAR(100) NOT NULL,
-  `Sexo_Usuario` ENUM("F", "M") NULL,
-  `Nivel_acesso` INT NOT NULL DEFAULT DEFAULT(1),
-  PRIMARY KEY (`id_Usuarios`),
-  INDEX `fk_Usuarios_Perfil_idx` (`id_Perfil` ASC),
-  CONSTRAINT `fk_Usuarios_Perfil`
-    FOREIGN KEY (`id_Perfil`)
-    REFERENCES `New_TCC`.`Perfil` (`id_Perfil`)
-    ON DELETE RESTRICT
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `New_TCC`.`Cursos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `New_TCC`.`Cursos` (
-  `id_Cursos` INT NOT NULL AUTO_INCREMENT,
-  `Nome_Curso` VARCHAR(45) NOT NULL,
-  `Horario_Curso` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_Cursos`))
-ENGINE = InnoDB;
+  `Data_Atualizacao_Perfil` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id_Perfil`),
+  INDEX `fk_Perfil_Usuario_idx` (`Id_Usuario` ASC),
+  CONSTRAINT `fk_Perfil_Usuario`
+    FOREIGN KEY (`Id_Usuario`)
+    REFERENCES `New_TCC`.`Usuario` (`Id_Usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
 -- Table `New_TCC`.`Postagem`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `New_TCC`.`Postagem` (
-  `id_Postagem` INT NOT NULL AUTO_INCREMENT,
-  `id_Usuarios` INT NOT NULL,
+  `Id_Postagem` INT NOT NULL AUTO_INCREMENT,
+  `Id_Usuario` INT NOT NULL,
   `Legenda_postagem` TEXT NULL,
   `Localizacao_midia_Postagem` VARCHAR(256) NULL,
   `Data_postagem` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_Postagem`),
-  INDEX `fk_Usuarios_Postagem_idx` (`id_Usuarios` ASC),
+  PRIMARY KEY (`Id_Postagem`),
+  INDEX `fk_Usuarios_Postagem_idx` (`Id_Usuario` ASC),
   CONSTRAINT `fk_Usuarios_Postagem`
-    FOREIGN KEY (`id_Usuarios`)
-    REFERENCES `New_TCC`.`Usuarios` (`id_Usuarios`)
+    FOREIGN KEY (`Id_Usuario`)
+    REFERENCES `New_TCC`.`Usuario` (`Id_Usuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+;
 
 
 -- -----------------------------------------------------
--- Table `New_TCC`.`Curtidas`
+-- Table `New_TCC`.`Curtida`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `New_TCC`.`Curtidas` (
-  `id_Curtidas` INT NOT NULL AUTO_INCREMENT,
-  `id_Usuarios` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `New_TCC`.`Curtida` (
+  `id_Curtida` INT NOT NULL AUTO_INCREMENT,
+  `id_Usuario` INT NOT NULL,
   `id_Postagem` INT NOT NULL,
   `Data_curtida` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_Curtidas`),
-  INDEX `fk_Usuarios_Curtidas_idx` (`id_Usuarios` ASC),
+  PRIMARY KEY (`id_Curtida`),
+  INDEX `fk_Usuarios_Curtidas_idx` (`id_Usuario` ASC),
   INDEX `fk_Postagem_Curtidas_idx` (`id_Postagem` ASC),
   CONSTRAINT `fk_Usuarios_Curtidas`
-    FOREIGN KEY (`id_Usuarios`)
-    REFERENCES `New_TCC`.`Usuarios` (`id_Usuarios`)
+    FOREIGN KEY (`id_Usuario`)
+    REFERENCES `New_TCC`.`Usuario` (`Id_Usuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Postagem_Curtidas`
     FOREIGN KEY (`id_Postagem`)
-    REFERENCES `New_TCC`.`Postagem` (`id_Postagem`)
+    REFERENCES `New_TCC`.`Postagem` (`Id_Postagem`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+;
 
 
 -- -----------------------------------------------------
--- Table `New_TCC`.`Comentarios`
+-- Table `New_TCC`.`Comentario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `New_TCC`.`Comentarios` (
-  `id_Comentarios` INT NOT NULL AUTO_INCREMENT,
-  `id_Usuarios` INT NOT NULL,
-  `id_Postagem` INT NOT NULL,
-  `Comentario` VARCHAR(256) NULL,
+CREATE TABLE IF NOT EXISTS `New_TCC`.`Comentario` (
+  `Id_Comentario` INT NOT NULL AUTO_INCREMENT,
+  `Id_Usuario` INT NOT NULL,
+  `Id_Postagem` INT NOT NULL,
+  `Comentario_Postado` VARCHAR(256) NULL,
   `Data_Comentario` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_Comentarios`),
-  INDEX `fk_Usuarios_Comentarios_idx` (`id_Usuarios` ASC),
-  INDEX `fk_Postagem_Comentarios_idx` (`id_Postagem` ASC),
+  PRIMARY KEY (`Id_Comentario`),
+  INDEX `fk_Usuarios_Comentarios_idx` (`Id_Usuario` ASC),
+  INDEX `fk_Postagem_Comentarios_idx` (`Id_Postagem` ASC),
   CONSTRAINT `fk_Usuarios_Comentarios`
-    FOREIGN KEY (`id_Usuarios`)
-    REFERENCES `New_TCC`.`Usuarios` (`id_Usuarios`)
+    FOREIGN KEY (`Id_Usuario`)
+    REFERENCES `New_TCC`.`Usuario` (`Id_Usuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Postagem_Comentarios`
-    FOREIGN KEY (`id_Postagem`)
-    REFERENCES `New_TCC`.`Postagem` (`id_Postagem`)
+    FOREIGN KEY (`Id_Postagem`)
+    REFERENCES `New_TCC`.`Postagem` (`Id_Postagem`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+;
 
 
 -- -----------------------------------------------------
--- Table `New_TCC`.`Professores`
+-- Table `New_TCC`.`Professor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `New_TCC`.`Professores` (
-  `id_Professores` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `New_TCC`.`Professor` (
+  `Id_Professor` INT NOT NULL AUTO_INCREMENT,
   `Nome_Professor` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_Professores`),
+  PRIMARY KEY (`Id_Professor`),
   UNIQUE INDEX `Nome_Professor_UNIQUE` (`Nome_Professor` ASC))
-ENGINE = InnoDB;
+;
 
 
 -- -----------------------------------------------------
@@ -139,129 +149,124 @@ CREATE TABLE IF NOT EXISTS `New_TCC`.`Professor_Curso` (
   INDEX `fk_Professores_Prof_Cursos_idx` (`id_Professor` ASC),
   CONSTRAINT `fk_Cursos_Prof_Cursos`
     FOREIGN KEY (`id_Cursos`)
-    REFERENCES `New_TCC`.`Cursos` (`id_Cursos`)
+    REFERENCES `New_TCC`.`Curso` (`id_Curso`)
     ON DELETE RESTRICT
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Professores_Prof_Cursos`
     FOREIGN KEY (`id_Professor`)
-    REFERENCES `New_TCC`.`Professores` (`id_Professores`)
+    REFERENCES `New_TCC`.`Professor` (`Id_Professor`)
     ON DELETE RESTRICT
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
 -- Table `New_TCC`.`Matricula`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `New_TCC`.`Matricula` (
-  `id_Matricula` INT NOT NULL AUTO_INCREMENT,
-  `id_Usuario` INT NOT NULL,
-  `id_Cursos` INT NOT NULL,
-  PRIMARY KEY (`id_Matricula`),
-  INDEX `fk_Usuario_Matricula_idx` (`id_Usuario` ASC),
-  INDEX `fk_Cursos_Matricula_idx` (`id_Cursos` ASC),
+  `Id_Matricula` INT NOT NULL AUTO_INCREMENT,
+  `Id_Usuario` INT NOT NULL,
+  `Id_Curso` INT NOT NULL,
+  PRIMARY KEY (`Id_Matricula`),
+  INDEX `fk_Usuario_Matricula_idx` (`Id_Usuario` ASC),
+  INDEX `fk_Cursos_Matricula_idx` (`Id_Curso` ASC),
   CONSTRAINT `fk_Usuario_Matricula`
-    FOREIGN KEY (`id_Usuario`)
-    REFERENCES `New_TCC`.`Usuarios` (`id_Usuarios`)
+    FOREIGN KEY (`Id_Usuario`)
+    REFERENCES `New_TCC`.`Usuario` (`Id_Usuario`)
     ON DELETE RESTRICT
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Cursos_Matricula`
-    FOREIGN KEY (`id_Cursos`)
-    REFERENCES `New_TCC`.`Cursos` (`id_Cursos`)
+    FOREIGN KEY (`Id_Curso`)
+    REFERENCES `New_TCC`.`Curso` (`id_Curso`)
     ON DELETE RESTRICT
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
 -- Table `New_TCC`.`Materia`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `New_TCC`.`Materia` (
-  `id_Materia` INT NOT NULL AUTO_INCREMENT,
+  `Id_Materia` INT NOT NULL AUTO_INCREMENT,
   `Id_Professor` INT NOT NULL,
   `Nome_Materia` VARCHAR(45) NOT NULL,
   `Modulo_Materia` INT NOT NULL,
-  `id_professor` VARCHAR(45) NULL,
-  PRIMARY KEY (`id_Materia`),
+  PRIMARY KEY (`Id_Materia`),
   UNIQUE INDEX `Nome_Materia_UNIQUE` (`Nome_Materia` ASC),
   INDEX `fk_Materia_Professor_idx` (`Id_Professor` ASC),
   CONSTRAINT `fk_Materia_Professor`
     FOREIGN KEY (`Id_Professor`)
-    REFERENCES `New_TCC`.`Professores` (`id_Professores`)
+    REFERENCES `New_TCC`.`Professor` (`Id_Professor`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `New_TCC`.`Materia_Cursos`
+-- Table `New_TCC`.`Materia_Curso`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `New_TCC`.`Materia_Cursos` (
-  `Id_Materia_Cursos` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `New_TCC`.`Materia_Curso` (
+  `Id_Materia_Curso` INT NOT NULL AUTO_INCREMENT,
   `Id_Materia` INT NOT NULL,
-  `Id_Cursos` INT NOT NULL,
-  PRIMARY KEY (`Id_Materia_Cursos`),
+  `Id_Curso` INT NOT NULL,
+  PRIMARY KEY (`Id_Materia_Curso`),
   INDEX `fk_MateCursos_Materia_idx` (`Id_Materia` ASC),
-  INDEX `fk_MateCursos_Cursos_idx` (`Id_Cursos` ASC),
+  INDEX `fk_MateCursos_Cursos_idx` (`Id_Curso` ASC),
   CONSTRAINT `fk_MateCursos_Materia`
     FOREIGN KEY (`Id_Materia`)
-    REFERENCES `New_TCC`.`Materia` (`id_Materia`)
+    REFERENCES `New_TCC`.`Materia` (`Id_Materia`)
     ON DELETE RESTRICT
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_MateCursos_Cursos`
-    FOREIGN KEY (`Id_Cursos`)
-    REFERENCES `New_TCC`.`Cursos` (`id_Cursos`)
+    FOREIGN KEY (`Id_Curso`)
+    REFERENCES `New_TCC`.`Curso` (`id_Curso`)
     ON DELETE RESTRICT
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `New_TCC`.`Conexoes`
+-- Table `New_TCC`.`Conexao`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `New_TCC`.`Conexoes` (
+CREATE TABLE IF NOT EXISTS `New_TCC`.`Conexao` (
+  `Id_Conexao` INT NOT NULL,
   `Solicitacao_Enviada` INT NOT NULL,
   `Solicitacao_Solicitada` INT NOT NULL,
   `Data_Conexao` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   INDEX `fk_Amizade_Usuarios_idx` (`Solicitacao_Solicitada` ASC),
   INDEX `fk_Amizade_Enviada_idx` (`Solicitacao_Enviada` ASC),
+  PRIMARY KEY (`Id_Conexao`),
   CONSTRAINT `fk_Amizade_Solicitada`
     FOREIGN KEY (`Solicitacao_Solicitada`)
-    REFERENCES `New_TCC`.`Usuarios` (`id_Usuarios`)
+    REFERENCES `New_TCC`.`Usuario` (`Id_Usuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Amizade_Enviada`
     FOREIGN KEY (`Solicitacao_Enviada`)
-    REFERENCES `New_TCC`.`Usuarios` (`id_Usuarios`)
+    REFERENCES `New_TCC`.`Usuario` (`Id_Usuario`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `New_TCC`.`Mensagens_Privadas`
+-- Table `New_TCC`.`Mensagem_Privada`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `New_TCC`.`Mensagens_Privadas` (
-  `Id_Mensagens_Privadas` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `New_TCC`.`Mensagem_Privada` (
+  `Id_Mensagem_Privada` INT NOT NULL AUTO_INCREMENT,
   `Id_Usuario_Emisssor` INT NOT NULL,
   `Id_Usuario_Receptor` INT NOT NULL,
   `Mensagem` TEXT NOT NULL,
   `Localizacao_Midia` TEXT NULL,
   `Data_Mensagem` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`Id_Mensagens_Privadas`),
+  PRIMARY KEY (`Id_Mensagem_Privada`),
   INDEX `fk_Usuario_Emissor_idx` (`Id_Usuario_Emisssor` ASC),
   INDEX `fk_Usuario_Receptor_idx` (`Id_Usuario_Receptor` ASC),
   CONSTRAINT `fk_Usuario_Emissor`
     FOREIGN KEY (`Id_Usuario_Emisssor`)
-    REFERENCES `New_TCC`.`Usuarios` (`id_Usuarios`)
+    REFERENCES `New_TCC`.`Usuario` (`Id_Usuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Usuario_Receptor`
     FOREIGN KEY (`Id_Usuario_Receptor`)
-    REFERENCES `New_TCC`.`Usuarios` (`id_Usuarios`)
+    REFERENCES `New_TCC`.`Usuario` (`Id_Usuario`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
@@ -278,85 +283,86 @@ CREATE TABLE IF NOT EXISTS `New_TCC`.`Notificacao` (
   INDEX `fk_Usuario_Notificacao_idx` (`Id_Usuario` ASC),
   CONSTRAINT `fk_Usuario_Notificacao`
     FOREIGN KEY (`Id_Usuario`)
-    REFERENCES `New_TCC`.`Usuarios` (`id_Usuarios`)
+    REFERENCES `New_TCC`.`Usuario` (`Id_Usuario`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `New_TCC`.`Grupos`
+-- Table `New_TCC`.`Grupo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `New_TCC`.`Grupos` (
-  `Id_Grupos` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `New_TCC`.`Grupo` (
+  `Id_Grupo` INT NOT NULL AUTO_INCREMENT,
   `Id_Criador_Professor` INT NOT NULL,
-  `Nome_Grupos` VARCHAR(50) NOT NULL,
-  `Descricao_Grupos` VARCHAR(50) NULL,
+  `Nome_Grupo` VARCHAR(50) NOT NULL,
+  `Descricao_Grupo` VARCHAR(50) NULL,
   `localizacao_Foto` TEXT NULL,
   `Data_Criacao` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   INDEX `fk_Grupos_Professores_idx` (`Id_Criador_Professor` ASC),
-  PRIMARY KEY (`Id_Grupos`),
+  PRIMARY KEY (`Id_Grupo`),
   CONSTRAINT `fk_Grupos_Professores`
     FOREIGN KEY (`Id_Criador_Professor`)
-    REFERENCES `New_TCC`.`Professores` (`id_Professores`)
+    REFERENCES `New_TCC`.`Professor` (`Id_Professor`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `New_TCC`.`Integrantes_Grupo`
+-- Table `New_TCC`.`Integrante_Grupo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `New_TCC`.`Integrantes_Grupo` (
-  `Id_Grupos_Usuarios` INT NOT NULL AUTO_INCREMENT,
-  `Id_Grupos` INT NOT NULL,
-  `Id_Usuarios` INT NOT NULL,
-  `Id_Professores` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `New_TCC`.`Integrante_Grupo` (
+  `Id_Intregante_Grupo` INT NOT NULL AUTO_INCREMENT,
+  `Id_Grupo` INT NOT NULL,
+  `Id_Usuario` INT NULL,
+  `Id_Professor` INT NULL,
   `Data_Entrada` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`Id_Grupos_Usuarios`),
-  INDEX `fk_Integrantes_Grupos_idx` (`Id_Grupos` ASC),
-  INDEX `fk_Integrantes_Usuarios_idx` (`Id_Usuarios` ASC),
-  INDEX `fk_Integrantes_Professores_idx` (`Id_Professores` ASC),
+  PRIMARY KEY (`Id_Intregante_Grupo`),
+  INDEX `fk_Integrantes_Grupos_idx` (`Id_Grupo` ASC),
+  INDEX `fk_Integrantes_Usuarios_idx` (`Id_Usuario` ASC),
+  INDEX `fk_Integrantes_Professores_idx` (`Id_Professor` ASC),
   CONSTRAINT `fk_Integrantes_Grupos`
-    FOREIGN KEY (`Id_Grupos`)
-    REFERENCES `New_TCC`.`Grupos` (`Id_Grupos`)
+    FOREIGN KEY (`Id_Grupo`)
+    REFERENCES `New_TCC`.`Grupo` (`Id_Grupo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Integrantes_Usuarios`
-    FOREIGN KEY (`Id_Usuarios`)
-    REFERENCES `New_TCC`.`Usuarios` (`id_Usuarios`)
+    FOREIGN KEY (`Id_Usuario`)
+    REFERENCES `New_TCC`.`Usuario` (`Id_Usuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Integrantes_Professores`
-    FOREIGN KEY (`Id_Professores`)
-    REFERENCES `New_TCC`.`Professores` (`id_Professores`)
+    FOREIGN KEY (`Id_Professor`)
+    REFERENCES `New_TCC`.`Professor` (`Id_Professor`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `New_TCC`.`Anexos`
+-- Table `New_TCC`.`Anexo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `New_TCC`.`Anexos` (
-  `Id_Anexos` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `New_TCC`.`Anexo` (
+  `Id_Anexo` INT NOT NULL AUTO_INCREMENT,
   `Id_Postagem` INT NULL,
-  `Id_Mensagens_Privadas` INT NULL,
+  `Id_Mensagens_Privada` INT NULL,
   `Nome_Arquivo` VARCHAR(255) NOT NULL,
   `Tipo_Anexo` VARCHAR(50) NOT NULL,
   `Caminho_Anexo` VARCHAR(255) NOT NULL,
   `Data_Anexo` TIMESTAMP NOT NULL,
-  PRIMARY KEY (`Id_Anexos`),
+  PRIMARY KEY (`Id_Anexo`),
   INDEX `fk_Anexos_Postagem_idx` (`Id_Postagem` ASC),
-  INDEX `fk_Anexos_Mensagens_Privadas_idx` (`Id_Mensagens_Privadas` ASC),
+  INDEX `fk_Anexos_Mensagens_Privadas_idx` (`Id_Mensagens_Privada` ASC),
   CONSTRAINT `fk_Anexos_Postagem`
     FOREIGN KEY (`Id_Postagem`)
-    REFERENCES `New_TCC`.`Postagem` (`id_Postagem`)
+    REFERENCES `New_TCC`.`Postagem` (`Id_Postagem`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Anexos_Mensagens_Privadas`
-    FOREIGN KEY (`Id_Mensagens_Privadas`)
-    REFERENCES `New_TCC`.`Mensagens_Privadas` (`Id_Mensagens_Privadas`)
+    FOREIGN KEY (`Id_Mensagens_Privada`)
+    REFERENCES `New_TCC`.`Mensagem_Privada` (`Id_Mensagem_Privada`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
